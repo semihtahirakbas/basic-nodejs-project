f7825d8fe456e193405bb0505d22dbd9d0868ed4
Tahir Akbas <41877595+semihtahirakbas@users.noreply.github.com>
2025-11-04 10:38:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!');
+  res.end('Hello, world! v2');
 });
 
 server.listen(3000, () => {
